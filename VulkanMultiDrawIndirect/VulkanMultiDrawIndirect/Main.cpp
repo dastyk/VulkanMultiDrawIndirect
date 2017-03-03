6a573280efbb2880f8a649467bcb2a1cf1e76c15
dastyk <dastykill@gmail.com>
2017-03-03 14:10:11 +0100

    try catch

--- VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Main.cpp
+++ VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Main.cpp
@@ -1,12 +1,27 @@
 
 #include <SDL.h>
+#include "Scene.h"
+#include <stdexcept>
 
 int main(int argc, char** argv)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		exit(1);
+
+	try
+	{
 
 
-	
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+			exit(1);
+
+		Scene scene;
+		scene.Init();
+		scene.Start();
+		scene.Shutdown();
+
+	}
+	catch (const std::runtime_error& err)
+	{
+
+	}
 	return 0;
 }
