f0e8af7444b34876b05a825f7772c870e6902533
dastyk <dastykill@gmail.com>
2017-03-07 09:20:22 +0100

    CPU timer

---
+++ VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Timer.cpp
@@ -0,0 +1,125 @@
+#include "Timer.h"
+
+
+
+Timer::Timer() : _secondsPerCount(0.0), _deltaTime(-1.0), _baseTime(0), _pausedTime(0), _prevTime(0), _currTime(0), _stopped(false), _mspf(0.0), _fps(0), _frameCount(0), _timeElapsed(0.0f), _stopTime(0)
+{
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)& countsPerSec);
+	_secondsPerCount = 1.0 / (double)countsPerSec;
+}
+
+
+Timer::~Timer()
+{
+}
+
+const float Timer::DeltaTime() const
+{
+	return (float)_deltaTime;
+}
+
+const float Timer::TotalTime() const
+{
+	if (_stopped)
+		return (float)(((_stopTime - _pausedTime) - _baseTime)*_secondsPerCount);
+	else
+		return (float)(((_currTime - _pausedTime) - _baseTime)*_secondsPerCount);
+}
+
+const unsigned int Timer::GetFps() const
+{
+	return _fps;
+}
+
+const float Timer::GetMspf() const
+{
+	return (float)_mspf;
+}
+
+const void Timer::Reset()
+{
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)& countsPerSec);
+	_secondsPerCount = 1.0 / (double)countsPerSec;
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+	_baseTime = currTime;
+	_prevTime = currTime;
+	_stopTime = 0;
+	_stopped = false;
+	_frameCount = 0;
+	_timeElapsed = 0.0f;
+	_mspf = 0.0;
+	_fps = 0;
+	return void();
+}
+
+const void Timer::Start()
+{
+	__int64 startTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+
+	if (_stopped)
+	{
+		_pausedTime += startTime - _stopTime;
+
+		_prevTime = startTime;
+
+		_stopTime = 0;
+		_stopped = false;
+	}
+	return void();
+}
+
+const void Timer::Stop()
+{
+	if (!_stopped)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+		_stopTime = currTime;
+		_stopped = true;
+	}
+	return void();
+}
+
+const void Timer::Tick()
+{
+	if (_stopped)
+	{
+		_deltaTime = 0.0;
+		return;
+	}
+
+	// Get time this frame
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	_currTime = currTime;
+
+	// Time diff between this and previous frame.
+	_deltaTime = (_currTime - _prevTime)*_secondsPerCount;
+
+	// Prepare for next frame
+	_prevTime = _currTime;
+
+	if (_deltaTime < 0.0)
+		_deltaTime = 0.0;
+
+
+
+	_frameCount++;
+
+	if ((TotalTime() - _timeElapsed) >= 1.0f)
+	{
+		_fps = _frameCount;
+		_mspf = 1000.0f / (float)_fps;
+
+
+		_frameCount = 0;
+		_timeElapsed += 1.0f;
+	}
+	return void();
+}
---
+++ VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Timer.h
@@ -0,0 +1,40 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#pragma once
+//////////////
+// Includes //
+//////////////
+#include <Windows.h>
+class Timer
+{
+public:
+	Timer();
+	~Timer();
+
+	const float DeltaTime()const;
+	const float TotalTime()const;
+	const unsigned int GetFps()const;
+	const float GetMspf()const;
+	const void Reset();
+	const void Start();
+	const void Stop();
+	const void Tick();
+
+private:
+	double _secondsPerCount;
+	double _deltaTime;
+	__int64 _pausedTime;
+	__int64 _baseTime;
+	__int64 _stopTime;
+	__int64 _prevTime;
+	__int64 _currTime;
+	double _mspf;
+	unsigned int _fps;
+	unsigned int _frameCount;
+	float _timeElapsed;
+
+	bool _stopped;
+};
+
+#endif
