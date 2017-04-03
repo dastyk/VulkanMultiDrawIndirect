e3fd1b982084703425efc753840e53fdf59dc123
Andreas Flöjt <andenzz@hotmail.com>
2017-04-03 14:49:50 +0200

    Meshes use corresponding transform when rendering

--- VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Scene.cpp
+++ VulkanMultiDrawIndirect/VulkanMultiDrawIndirect/Scene.cpp
@@ -1,5 +1,7 @@
 #include "Scene.h"
 
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
 #include <SDL.h>
 
 Scene::Scene(Renderer& renderer) : _renderer(renderer)
@@ -19,10 +21,10 @@ const void Scene::Init()
 	_renderer.SetProjectionMatrix(p);
 
 
-	_CreateObject("../Assets/Meshes/deer-obj.obj", "../Assets/Textures/deer texture.tga", glm::vec3(-10, 0, 0));
+	_CreateObject("../Assets/Meshes/deer-obj.obj", "../Assets/Textures/deer texture.tga", glm::translate(glm::mat4(), glm::vec3(-20, 0, 0)));
 	//_CreateObject("../Assets/Meshes/bear-obj.obj", "../Assets/Textures/bear.tga");
 	//_CreateObject("../Assets/Meshes/boar-obj.obj", "../Assets/Textures/boar.tga");
-	_CreateObject("../Assets/Meshes/wolf-obj.obj", "../Assets/Textures/wolf texture.tga", glm::vec3(0, 0, 0));
+	_CreateObject("../Assets/Meshes/wolf-obj.obj", "../Assets/Textures/wolf texture.tga", glm::mat4());
 
 	for (auto& o : _objects)
 		_renderer.Submit(o.mesh, o.texture, o.translation);
@@ -42,7 +44,7 @@ const void Scene::Shutdown()
 	return void();
 }
 
-const void Scene::_CreateObject(const char * mesh, const char * texture, const glm::vec3& translation)
+const void Scene::_CreateObject(const char * mesh, const char * texture, const glm::mat4& translation)
 {
 	_objects.push_back({
 		_renderer.CreateMesh(mesh),
